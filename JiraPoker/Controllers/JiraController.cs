48c482e695550d59d3bbbf70de500b2fa01876b5
aleksandar.konatar <aleksandar.k03@gmail.com>
2022-05-06 19:49:02 +0200

    Configuration

--- JiraPoker/Controllers/JiraController.cs
+++ JiraPoker/Controllers/JiraController.cs
@@ -4,6 +4,26 @@ namespace JiraPoker.Controllers;
 
 public class JiraController : Controller
 {
+    private IConfiguration _configuration;
+
+    public JiraController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IActionResult Test()
+    {
+        return Content(_configuration.GetSection("Jira").GetSection("Authentication").GetValue<string>("Url"));
+    }
+
+    public IActionResult Test2()
+    {
+        var result = Environment.GetEnvironmentVariable("test");
+        if (string.IsNullOrEmpty(result))
+            return Content("Null");
+        return Content(result);
+    }
+    
     public async Task<IActionResult> Callback()
     {
         var response = Request.QueryString
